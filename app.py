e61997ceea8164eac033c26713a5b58baad19e38
Henry <henryku90911@gmail.com>
2025-01-20 16:21:36 +0800

    init

--- app.py
+++ app.py
@@ -17,7 +17,10 @@ from linebot.v3.webhooks import (
     MessageEvent,
     TextMessageContent
 )
-from response import get_answer
+import re
+from langchain_ollama import OllamaLLM
+from langchain.prompts import PromptTemplate
+from opencc import OpenCC
 import os
 
 app = Flask(__name__)
@@ -59,6 +62,39 @@ def handle_message(event):
                 messages=[TextMessage(text=response)]
             )
         )
+def contains_simplified_chinese(text):
+    # 使用 Unicode 範圍檢查是否包含簡體中文字符
+    simplified_chinese_pattern = re.compile(r"[\u4e00-\u9fff]")
+    return bool(simplified_chinese_pattern.search(text))
+
+def get_answer(question: str):
+    llm = OllamaLLM(model="llama3.1:8b-instruct-q8_0", temperature=0.1, base_url="http://localhost:11436")
+    casual_prompt = PromptTemplate(
+        input_variables=["input"],
+        template="""
+        You are a friendly assistant for **Bestat Biotechnology Co., Ltd.**. Please adhere to the following guidelines:  
+
+        1. Respond to the user in a kind and friendly manner.  
+        2. Answer the user's question directly.  
+        3. Do not under any circumstances ask the user questions or seek clarification. Provide only direct answers or responses.
+        4. Respond in the language used by the user.
+
+
+        **User's Question:**
+        {input}  
+
+        Begin response to the user's question: (The generated response must not include above paragraph)
+        """
+    )
+    casual_chain = casual_prompt | llm
+    response = casual_chain.invoke({"input": question})
+
+    if contains_simplified_chinese(response):
+        converter = OpenCC('s2t')  # 簡體轉繁體
+        response = converter.convert(response)
+
+    return response
 
 if __name__ == "__main__":
-    app.run(host='0.0.0.0', port=int(os.getenv('PORT', 5000)))
+    # app.run(host='0.0.0.0', port=int(os.getenv('PORT', 5000)))
+    app.run(debug=False,host='localhost',port=80)
